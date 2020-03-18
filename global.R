c792cc5120a622a7110647a32bbda021aad839ba
ashvin14 <yashkhrnr@gmail.com>
2020-03-18 16:43:29 -0700

    please everyone follow instructions given within file

--- global.R
+++ global.R
@@ -1,11 +1,5 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# DO NOT TOUCH THIS FILE!!!
+# IT IS NOT SUPPOSED TO BE TAMPERED 
 
 library(shiny)
 source("./server/server.R")
