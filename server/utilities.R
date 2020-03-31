0a396b30c9271f2f50221a5dcbe02e4471a9b72c
Soumyajeet-Patra <soumyajeet.patra@gmail.com>
2020-03-30 22:02:17 -0700

    Added Quantity column and repitition filter

--- server/utilities.R
+++ server/utilities.R
@@ -4,3 +4,35 @@ get_ingredients <- function(molten_dataframe, recipe_title) {
   return(result$ingredients)
 }
 
+
+deleteButtonColumn <- function(df, id, ...) {
+  # function to create one action button as string
+  f <- function(i) {
+    as.character(actionButton(paste(id, i, sep="_"), label = NULL, icon = icon('trash'),
+                              onclick = 'Shiny.setInputValue(\"deletePressed\",  this.id, {priority: "event"})'))
+  }
+
+  df <- data.frame(df)
+  names(df) <- c("Ingredients", "No of Portions")
+  deleteCol <- unlist(lapply(seq_len(nrow(df)), f))
+
+  # Return a data table
+  DT::datatable(cbind(df, Delete = deleteCol),
+                # Need to disable escaping for html as string to work
+                escape = FALSE,
+                rownames = FALSE,
+                options = list(
+                  # Disable sorting for the delete column
+                  columnDefs = list(list(targets = 1, sortable = FALSE))
+                ))
+}
+
+
+parseDeleteEvent <- function(idstr) {
+  res <- as.integer(sub(".*_([0-9]+)", "\\1", idstr))
+  if (! is.na(res)) res
+}
+
+
+
+
