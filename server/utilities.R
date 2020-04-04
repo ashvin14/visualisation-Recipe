1f96be46a7a52cfea2d90db7a80ef4096b86d3a6
ashvin14 <yashkhrnr@gmail.com>
2020-04-03 17:24:54 -0700

    quantity working

--- server/utilities.R
+++ server/utilities.R
@@ -5,7 +5,6 @@ get_ingredients <- function(molten_dataframe, recipe_title) {
 }
 
 
-
 deleteButtonColumn <- function(df, id, ...) {
   # function to create one action button as string
   f <- function(i) {
@@ -14,38 +13,23 @@ deleteButtonColumn <- function(df, id, ...) {
   }
   
   df <- data.frame(df)
-  names(df) <- "Ingredients"
+  names(df) <- c("Ingredients", "No of Portions")
   deleteCol <- unlist(lapply(seq_len(nrow(df)), f))
   
   # Return a data table
   DT::datatable(cbind(df, Delete = deleteCol),
                 # Need to disable escaping for html as string to work
                 escape = FALSE,
+                rownames = FALSE,
                 options = list(
                   # Disable sorting for the delete column
                   columnDefs = list(list(targets = 1, sortable = FALSE))
                 ))
 }
 
-get_constituents <- function(molten_dataframe, recipe_titles) {
-  result <-
-    recipe_dataset[recipe_dataset$title %in% recipe_titles$Recipes, ]
-  
-  consitituents <- tibble(
-    "title" = recipe_titles$Recipes,
-    "calories" = result$calories,
-    "protein" = result$protein,
-    "fat" = result$fat,
-    "sodium" = result$sodium
-  )
-  print(consitituents)
-  return(consitituents)
-}
-
-
 
 parseDeleteEvent <- function(idstr) {
   res <- as.integer(sub(".*_([0-9]+)", "\\1", idstr))
-  if (!is.na(res))
-    res
+  if (! is.na(res)) res
 }
+
